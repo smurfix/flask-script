15514d1c8979d15fb60d6904affb64530eb0cfa2
Dan Jacob <danjac354@gmail.com>
2010-07-04 14:26:44 +0100

    more rough ideas

--- flaskext/script.py
+++ flaskext/script.py
@@ -2,13 +2,18 @@
 Basic idea:
 # manage.py
 
-from flaskext.script import register_script
+from flaskext.script import Manager
 
-def create_app(*args, **options):
+def create_app(config=None, *args, **options):
     # *args and **kwargs are command-line options
-    return Flask(__name__)
+    app = Flask(__name__)
+    if config is not None:
+        app.config.from_object(config)
+    return app
 
-@register_script(create_app)
+manager = Manager(create_app)
+
+@manager.register('do_something')
 def do_something(name, **options):
     print name
 
@@ -17,12 +22,36 @@ def do_something(name, **options):
 >>> python manage.py do_something --name=hello
 ... "hello"
 
-from flaskext.script import make_shell
 
 def make_shell_ctx(app):
-    return dict(db=db)
+    return dict(db=db, app=app)
 
-shell = make_shell(create_app, make_shell_ctx, title="My App shell")
-runserver = make_runserver(create_app)
+manager.configure_shell(ctx=make_shell_ctx, title="My App Context")
+
+if __name__ == "__main__":
+    manager.run()
 
 """
+import sys
+import code
+
+from optparse import OptionParser, make_option
+
+class Manager(object):
+
+    def __init__(self, app_factory):
+        self.app_factory = app_factory
+        self._scripts = dict()
+    
+    def register(self, func, name=None):
+        if name is None:
+            name = func.__name__
+        self._scripts[name] = func
+
+    def run(self):
+
+        script = self._scripts[sys.argv[1]]
+        # get option list
+        parser = self.create_parser()
+        options, args = parser.parse_args(argv[2:])
+        script(*args, **options)
