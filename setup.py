debca6a4d5cb01512dc6fa9bc0b8a414f8f9b5c8
Sean Lynch <sean.lynch@sbcs.com>
2012-09-03 22:58:12 -0400

    Hack to prevent TypeError: 'NoneType' object is not callable error on exit of 'python setup.py test'

--- setup.py
+++ setup.py
@@ -13,6 +13,14 @@ Links
 """
 from setuptools import setup
 
+# Hack to prevent stupid TypeError: 'NoneType' object is not callable error on
+# exit of python setup.py test # in multiprocessing/util.py _exit_function when
+# running python setup.py test (see
+# https://github.com/pypa/virtualenv/pull/259)
+try:
+    import multiprocessing
+except ImportError:
+    pass
 
 setup(
     name='Flask-Script',
