725faaf98acd1d05fdabc62c387934c6b120c306
Matthias Urlichs <matthias@urlichs.de>
2014-03-04 04:30:58 +0100

    Fix option list processing

--- flask_script/commands.py
+++ flask_script/commands.py
@@ -96,10 +96,12 @@ class Command(object):
     :param func:  Initialize this command by introspecting the function.
     """
 
-    option_list = []
+    option_list = ()
 
     def __init__(self, func=None):
         if func is None:
+            if not self.option_list:
+                self.option_list = []
             return
 
         args, varargs, keywords, defaults = inspect.getargspec(func)
