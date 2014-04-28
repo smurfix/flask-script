0224b54e61bd6a10b147c4cd8a3e23de5a7fe33f
Matthias Urlichs <matthias@urlichs.de>
2014-04-28 17:47:33 +0200

    New version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires = ['Flask']
 
 setup(
     name='Flask-Script',
-    version='2.0.0',
+    version='2.0.1',
     url='http://github.com/smurfix/flask-script',
     license='BSD',
     author='Dan Jacob',
